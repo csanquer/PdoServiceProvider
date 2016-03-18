7395bace8ab62f4221dee8ba79add419b2979f59
Brandon Mueller <bmueller@k12servicesinc.com>
2016-03-18 13:19:35 -0400

    Fixes issue with configuration in silex constructor

--- tests/Provider/PDOServiceProviderTest.php
+++ tests/Provider/PDOServiceProviderTest.php
@@ -40,6 +40,48 @@ class PDOServiceProviderTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedAttributes, $app[$prefix.'.attributes']);
     }
 
+    public function testRegistrationDoesNotOverwriteConfig()
+    {
+        $pdoMock = $this->getMock(
+            '\\Csanquer\\Silex\\PdoServiceProvider\\Provider\\PDOServiceProvider',
+            array('getPdo'),
+            array('test_pdo')
+        );
+        $pdoMock->expects($this->once())->method('getPdo');
+        $config = $this->getTestConfig();
+        $app = new Application($config);
+        $app->register($pdoMock);
+        foreach ($config as $key => $value) {
+            $this->assertSame($value, $app[$key]);
+        }
+    }
+
+    protected function getTestConfig()
+    {
+        return array(
+            'test_pdo.server'   => array(
+                // PDO driver to use among : mysql, pgsql , oracle, mssql, sqlite, dblib
+                'driver'   => 'mysql',
+                'host'     => 'mock_host',
+                'dbname'   => 'mock_db',
+                'port'     => 3306,
+                'user'     => 'mock_user',
+                'password' => 'mock_password',
+            ),
+            // optional PDO attributes used in PDO constructor 4th argument driver_options
+            // some PDO attributes can be used only as PDO driver_options
+            // see http://www.php.net/manual/fr/pdo.construct.php
+            'test_pdo.options' => array(
+                \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+            ),
+            // optional PDO attributes set with PDO::setAttribute
+            // see http://www.php.net/manual/fr/pdo.setattribute.php
+            'test_pdo.attributes' => array(
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            ),
+        );
+    }
+
     public function providerConnection()
     {
         return array(
